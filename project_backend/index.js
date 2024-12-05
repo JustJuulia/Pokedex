73ee7562f900f4cd1a29ba5f85bbe4adfeb9bb3e
Julia <102248096+JustJuulia@users.noreply.github.com>
2024-12-05 12:56:55 +0100

    images now convert to base64

--- project_backend/index.js
+++ project_backend/index.js
@@ -4,15 +4,17 @@ const cors = require("cors");
 application.use(cors());
 const pokemon = require("./pokedex.json");
 const types = require("./types.json");
-const path = require('path')
-
+const path = require("path");
+const fs = require('fs');
 application.get("/types", (request, response) => {
-  response.send(types)
+  response.send(types);
 });
 application.get("/type/:typ", (request, response) => {
-  const picked_by_type = pokemon.filter((pokem) => pokem.type.includes(request.params.typ))
+  const picked_by_type = pokemon.filter((pokem) =>
+    pokem.type.includes(request.params.typ)
+  );
   response.send(picked_by_type);
-})
+});
 application.get("/search", (request, response) => {
   const val = request.query.val;
   const types = request.query.type;
@@ -31,21 +33,21 @@ application.get("/search", (request, response) => {
 });
 
 application.get("/images/:id", (request, response) => {
-  const id_p = request.params.id.padStart(3, '0');
-  const path_img = path.join(__dirname, 'images', `${id_p}.png`);
-  response.sendFile(path_img, (err) =>{
-    if (err) {
-      res.status(404).json({ error: `Image not found ${path_img}` });
-    }
-  })
+  const id_p = request.params.id.padStart(3, "0");
+  const path_img = path.join(__dirname, "images", `${id_p}.png`);
+
+  if (fs.existsSync(path_img)) {
+    response.sendFile(path_img);
+  } else {
+    response.status(404).send("Image not found");
+  }
 });
 
 application.get("/:id", (request, response) => {
-  let poki = pokemon.find(poke => poke.id === parseInt(request.params.id));
+  let poki = pokemon.find((poke) => poke.id === parseInt(request.params.id));
   if (poki) {
     response.send(poki);
   }
-})
-
+});
 
 application.listen(8000, () => console.log("servere started"));
